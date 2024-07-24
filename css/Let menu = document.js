a88b34102c5e77ce166fb7efff70c02bb1cdae68
HeinMinZaw95 <auntymarlaundry@gmail.com>
2024-07-25 00:06:16 +0630

    Delete css directory

--- css/Let menu = document.js
+++
@@ -1,7 +0,0 @@
-Let menu = document.querySelector('#menu-icon');
-Let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classlist.toggle('bx-x');
-    navbar.classlist.toggle('open')
-}
